0f08f14f5211a2c7c27049e84b21e35d1eb086dd
Kartik Patel <kartik@simformsolutions.com>
2023-02-24 08:24:37 +0530

    completed with touching

--- Prectical Exam.sql
+++ Prectical Exam.sql
@@ -1,9 +1,8 @@
--- user - user id, user_name, email, phone_number, address, pincode
-
-
 create database online_delivery_system;
-
 use online_delivery_system;
+
+
+-- user - user id, user_name, email, phone_number, address, pincode;
 
 create table `user` (
 `u_id` int not null auto_increment,
@@ -20,7 +19,7 @@ primary key (u_id)
 
  
 
--- product - product id, product name, product price, description, quantity
+-- product - product id, product name, product price, description, quantity;
 
 create table product(
 `p_id`int not null auto_increment,
@@ -32,8 +31,9 @@ primary key (p_id)
 );
 alter table product add column `quntity` int not null;
 
-delimiter //
 
+
+delimiter //
 create trigger `update_before_product` 
 before update on `product`
 for each row
@@ -59,8 +59,7 @@ delimiter  ;
 
 
   
-  -- order - order id, user_id, order_status, expected delivery date
--- order details - order id,
+  -- order - order id, user_id, order_status, expected delivery date;
 
 
 create table orders (
@@ -74,12 +73,11 @@ primary key (o_id),
 foreign key (u_id) references `user`(u_id)
 );
 alter table orders modify order_status varchar(20) not null default 'undelivered';
-
-
 alter table orders add column delivery_date datetime generated always as (DATE_ADD(order_date, interval 7 DAY)) stored;
 
-select * from orders;
 
+
+-- order details - order id,product_id,quntity,price;
 create table order_details(
 od_id int auto_increment,
 o_id int not null,
@@ -98,15 +96,11 @@ begin
 declare old_quntity int;
 insert into orders (`u_id`) value(user_id);
 insert into order_details (`o_id`,`p_id`,`quntity`,`price`) value((select last_insert_id()),product_id,now_quntity,(select price from product where `p_id`=product_id)*now_quntity);
-
- select `quntity` into old_quntity from `product` where `p_id`=product_id ;
-
-	update `product` set `quntity`=old_quntity-now_quntity where  `p_id`=product_id;
+select `quntity` into old_quntity from `product` where `p_id`=product_id ;
+update `product` set `quntity`=old_quntity-now_quntity where  `p_id`=product_id;
 end
-
-   //
- delimiter ;
-
+//
+delimiter ;
 
 
 delimiter //
@@ -116,6 +110,9 @@ update  orders set  order_status='delivered' where o_id=order_id;
 end 
 //
 delimiter ;
+
+
+-- inserting data in tables
 
 insert into user (name,street_number,address_line1,address_line2,city,state,country,pincode)
 values('deep',6,'suthar faliya','luna','padra','gujrat','india',391440);
@@ -186,12 +183,26 @@ call userorder(5,12,2);
 call userorder(6,13,5);
 call userorder(7,14,4);
 call userorder(1,14,3);
-
+call userorder(7,8,5);
+call userorder(6,9,3);
+call userorder(5,10,8);
+call userorder(3,12,2);
+call userorder(2,13,5);
 
 
 call changestatus(18);
 call changestatus(21);
 call changestatus(22);
+call changestatus(24);
+call changestatus(26);
+call changestatus(28);
+
+
+
+
+
+
+
 
 
 
@@ -215,7 +226,7 @@ select * from order_details;
 -- - Order Date
 -- - Expected delivery date (in days, i.e. within X days)
 
-select u.`name` , p.`product_name`, o.`order_date`,CONCAT( DATEDIFF(o.`delivery_date`, o.`order_date`) , ' Days'), o.`o_id` from  `user`  u   inner join `orders` o on u.u_id=o.u_id inner join order_details od on od.o_id=o.o_id inner join `product` p on p.p_id=od.p_id;
+select u.`name` , p.`product_name`, o.`order_date`,CONCAT( DATEDIFF(o.`delivery_date`, o.`order_date`) , ' Days') as 'Day left', o.`o_id` from  `user`  u   inner join `orders` o on u.u_id=o.u_id inner join order_details od on od.o_id=o.o_id inner join `product` p on p.p_id=od.p_id;
 
 
 -- 2. Create summary report which provide information about
@@ -231,16 +242,18 @@ select u.`name` ,p.product_name, o.order_date  from `user` u natural join orders
 
 -- - Top 5 active users (Users having most number of orders)
 
-select u.`u_id`,u.`name`,concat('Order ' ,count(u.u_id),' Times') from `user` u natural join orders o natural join order_details od inner join product p on od.p_id=p.p_id group by u.u_id limit 5;
+select u.`u_id`,u.`name`,concat('Order ' ,count(u.u_id),' Times') from `user` u natural join orders o natural join order_details od inner join product p on od.p_id=p.p_id group by (u.u_id) order by count(u.u_id) desc  limit 5 ;
 
 
 -- - Inactive users (Users who hasn’t done any order)
 
 select  u.`u_id`,u.`name` from `user` u where u.u_id not in (select distinct u_id from orders)  ;
 
--- Top 5 Most purchased products 
+-- Top 5 Most purchased products ;
 
-select p.product_name,count(od.quntity) from product p inner join order_details od on od.p_id=p.p_id group by od.p_id order by  count(od.quntity) desc limit 5;
+select * from order_details;
+
+select p.product_name,sum(od.quntity) from product p inner join order_details od on od.p_id=p.p_id group by od.p_id order by  sum(od.quntity) desc limit 5;
   
   
   -- Most expensive orders.
